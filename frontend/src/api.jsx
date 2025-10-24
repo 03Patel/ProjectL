4d399dc19a4e54ea269a728691ecb7a8b81eb160
Ganesh Ji Patel <161694533+03Patel@users.noreply.github.com>
2025-10-25 00:20:23 +0530

    Update api.jsx

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://projectl-1b8v.onrender.com/api";
 
 export const api = axios.create({ baseURL: BASE_URL });
 
